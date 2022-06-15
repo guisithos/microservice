ba96b2685b81051b27513ab7c3ae6ccf1e02e2df
Guilherme Thomas <gui.thomas@outlook.com>
2022-06-15 09:27:33 -0300

    Update main.rs

--- tiny_server/src/main.rs
+++ tiny_server/src/main.rs
@@ -1,18 +1,20 @@
 use std::fmt;
 use std::sync::{Arc, Mutex};
 use slab::Slab;
+use lazy_static::lazy_static;
 use futures::{future, Future};
 use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
+use regex::Regex;
 
 const INDEX: &str = r#"
 <!doctype html>
 <html>
     <head>
-        <title>Rust tinyServer</title>
+        <title>Rust Microservice</title>
     </head>
     <body>
-        <h3>Rust tinyServer</h3>
+        <h3>Rust Microservice</h3>
     </body>
 </html>
 "#;
@@ -29,70 +31,92 @@ impl fmt::Display for UserData {
 
 type UserDb = Arc<Mutex<Slab<UserData>>>;
 
-const USER_PATH: &str = "/user/";
+lazy_static! {
+    static ref INDEX_PATH: Regex = Regex::new("^/(index\\.html?)?$").unwrap();
+    static ref USER_PATH: Regex = Regex::new("^/user/((?P<user_id>\\d+?)/?)?$").unwrap();
+    static ref USERS_PATH: Regex = Regex::new("^/users/?$").unwrap();
+}
 
 fn microservice_handler(req: Request<Body>, user_db: &UserDb)
-    //Reference to shared state
+    //Referecent to shared state
     -> impl Future<Item=Response<Body>, Error=Error>
 {
     //Match expression detects returned method from req.method and URI from req.uri()
     let response = {
-        match (req.method(), req.uri().path()) {
-            (&Method::GET, "/") => {
+        let method = req.method();
+        let path = req.uri().path();
+        let mut users = user_db.lock().unwrap();
+        if INDEX_PATH.is_match(path) {
+            if method == &Method::GET {
                 Response::new(INDEX.into())
-            },
-            (method, path) if path.starts_with(USER_PATH) => {
-                let user_id = path.trim_start_matches(USER_PATH)
-                    .parse::<UserId>()
-                    .ok()
-                    .map(|x| x as usize);
-                let mut users = user_db.lock().unwrap();
-                match (method, user_id) {
-                    (&Method::GET, Some(id)) => {
-                        if let Some(data) = users.get(id) {
-                            Response::new(data.to_string().into())
-                       } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    (&Method::POST, None) => {
-                        let id = users.insert(UserData);
-                        Response::new(id.to_string().into())
-                    },
-                    (&Method::POST, Some(_)) => {
-                        response_with_code(StatusCode::BAD_REQUEST)
-                    },
-                    (&Method::PUT, Some(id)) => {
-                        if let Some(user) = users.get_mut(id) {
-                            *user = UserData;
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    (&Method::DELETE, Some(id)) => {
-                        if users.contains(id) {
-                            users.remove(id);
-                            response_with_code(StatusCode::OK)
-                        } else {
-                            response_with_code(StatusCode::NOT_FOUND)
-                        }
-                    },
-                    _ => {
-                        response_with_code(StatusCode::METHOD_NOT_ALLOWED)
-                    },
-                }
-            },
-            _ => {
-                response_with_code(StatusCode::NOT_FOUND)
-            },
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if USERS_PATH.is_match(path) {
+            if method == &Method::GET {
+                let list = users.iter()
+                    .map(|(id, _)| id.to_string())
+                    .collect::<Vec<String>>()
+                    .join(",");
+                Response::new(list.into())
+            } else {
+                response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+            }
+        } else if let Some(cap) = USER_PATH.captures(path) {
+            let user_id = cap.name("user_id").and_then(|m| {
+                m.as_str()
+                 .parse::<UserId>()
+                 .ok()
+                 .map(|x| x as usize)
+            });
+            match (method, user_id) {
+		//Add UserData instance to user DB, send user ID with ok status
+		(&Method::POST, None) => {
+                    let id = users.insert(UserData);
+                    Response::new(id.to_string().into())
+                },
+                (&Method::POST, Some(_)) => {
+                    response_with_code(StatusCode::BAD_REQUEST)
+                },
+		//From user DB get ID, if found convert to str, else 404
+                (&Method::GET, Some(id)) => {
+                    if let Some(data) = users.get(id) {
+                        Response::new(data.to_string().into())
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+                //get_mut search the user DB, deference * to replace data
+                (&Method::PUT, Some(id)) => {
+                    if let Some(user) = users.get_mut(id) {
+                        *user = UserData;
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+		//Check if slabs contains data, then remove w/ remove method.
+		//else 404. Need to check data b4 removing so it doesn't panic.
+                (&Method::DELETE, Some(id)) => {
+                    if users.contains(id) {
+                        users.remove(id);
+                        response_with_code(StatusCode::OK)
+                    } else {
+                        response_with_code(StatusCode::NOT_FOUND)
+                    }
+                },
+                _ => {
+                    response_with_code(StatusCode::METHOD_NOT_ALLOWED)
+                },
+            }
+        } else {
+            response_with_code(StatusCode::NOT_FOUND)
         }
     };
     future::ok(response)
 }
-
-//Fn creates empty responses, carrying out actions: expects status code
-//creates new responde builder, set status, adds empty body
+//FN creates empty responses, carrying out: expects status code, creates new 
+//response builder, set status, adds empty body.
 fn response_with_code(status_code: StatusCode) -> Response<Body> {
     Response::builder()
         .status(status_code)
